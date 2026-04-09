db52e6e5d60b2e7a8a35058287d0285266bbcba6
Jakub <jakubkromolowski@gmail.com>
2026-04-09 20:04:12 +0100

    Fixed stablization when the is no wall

--- motion.cpp
+++ motion.cpp
@@ -46,31 +46,40 @@ void driveForward(bool stabilize = true) {
         bool rightWall = right < OPEN_THRESHOLD;
 
         if (leftWall && rightWall) {
-
             float targetOffset = 2.0;
             error = (left - right) - targetOffset;
         }
-
         else if (leftWall) {
-
             float targetLeft = 10.0;
             error = (left - targetLeft);
         }
-
         else if (rightWall) {
-
             float targetRight = 10.0;
             error = -(right - targetRight);
         }
-
         else {
 
-            int straightCorrection = 15;
+            static long lastLeftTicks = 0;
+            static long lastRightTicks = 0;
 
-            leftSpeed  = baseLeft  + straightCorrection;
-            rightSpeed = baseRight - straightCorrection;
+            long leftTicks  = getLeftTicks();
+            long rightTicks = getRightTicks();
 
-            lastError = 0;
+            long dLeft  = leftTicks  - lastLeftTicks;
+            long dRight = rightTicks - lastRightTicks;
+
+            lastLeftTicks  = leftTicks;
+            lastRightTicks = rightTicks;
+
+            float errorEnc = (float)(dLeft - dRight);
+
+            float KpEnc = 1.5;
+            float correction = KpEnc * errorEnc;
+
+            correction = constrain(correction, -30, 30);
+
+            leftSpeed  = baseLeft  - correction;
+            rightSpeed = baseRight + correction;
 
             setMotorSpeed(leftSpeed, rightSpeed);
             leftMotorForward();
@@ -81,8 +90,7 @@ void driveForward(bool stabilize = true) {
         float derivative = error - lastError;
         float correction = Kp * error + Kd * derivative;
 
-        float maxCorrection = 50;
-        correction = constrain(correction, -maxCorrection, maxCorrection);
+        correction = constrain(correction, -50, 50);
 
         leftSpeed  = baseLeft  - correction;
         rightSpeed = baseRight + correction;
@@ -132,6 +140,14 @@ void updateMotion() {
             RobotState newDecision = FORWARD;
 
             if (left < OPEN_THRESHOLD && right < OPEN_THRESHOLD && front < 10) {
+
+                if (left > right) {
+                    turnAroundLeft = true;
+                } else {
+                    turnAroundLeft = false;
+                }
+
+                resetEncoders();
 
                 currentState = TURNING_AROUND;
                 return;
